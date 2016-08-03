358959fac689ce1a6deb8f77331fcc2694fef928
Aaron Hammer <athammer@buffalo.edu>
2016-08-03 22:50:26 +0000

    Forgot middleware

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(helmet());
 app.use(favicon('./public/img/favicon.png'));
 app.use(express.static(__dirname + '/public'));
+app.use(middlewares.prettifyDomain)
 
 
 require('./app/controllers/routes/routes.js')(app);
