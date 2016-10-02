eaf7ae2a4f878c8b407f09be0a78aead81f2e102
Aaron Hammer <athammer@buffalo.edu>
2016-10-02 19:19:57 +0000

    Large overhual, used more bootstrap elements for good or worse.

--- app/controllers/routes/routes.js
+++ app/controllers/routes/routes.js
@@ -9,6 +9,18 @@ module.exports = function(app){
         res.render('./pages/about.ejs');
     });
     
+    app.get('/projects/13chan', function(req, res) {
+        res.render('./pages/13chan.ejs');
+    });
+    
+    app.get('/projects/ELCE', function(req, res) {
+        res.render('./pages/ELCE.ejs');
+    });
+    
+    app.get('/projects/athammer', function(req, res) {
+        res.render('./pages/athammer.ejs');
+    });
+    
     app.get('/download', function(req, res) {
         var file = "public/pdf/Aaron Hammer's Resume.pdf"
         res.download(file); // Set disposition and send it.
