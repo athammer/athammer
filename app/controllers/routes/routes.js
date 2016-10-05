75836596306f73916a24812b908288d387eb5f3e
Aaron Hammer <athammer@buffalo.edu>
2016-10-05 02:24:17 +0000

    Updated projects.

--- app/controllers/routes/routes.js
+++ app/controllers/routes/routes.js
@@ -9,38 +9,46 @@ module.exports = function(app){
         res.render('./pages/about.ejs');
     });
     
+    
+    
+    
+    //PROJECTS
     app.get('/projects/13chan', function(req, res) {
-        res.render('./pages/13chan.ejs');
+        res.render('./pages/projects/13chan.ejs');
     });
     
     app.get('/projects/ELCE', function(req, res) {
-        res.render('./pages/ELCE.ejs');
+        res.render('./pages/projects/ELCE.ejs');
     });
     
     app.get('/projects/athammer', function(req, res) {
-        res.render('./pages/athammer.ejs');
+        res.render('./pages/projects/athammer.ejs');
     });
     
+    
+    //ABOUTME
     app.get('/aboutMe/hobbies', function(req, res) {
-        res.render('./pages/hobbies.ejs');
+        res.render('./pages/aboutMe/hobbies.ejs');
     });
     
     app.get('/aboutMe/education', function(req, res) {
-        res.render('./pages/education.ejs');
+        res.render('./pages/aboutMe/education.ejs');
     });
     
     app.get('/aboutMe/boy-scouts', function(req, res) {
-        res.render('./pages/boy-scouts.ejs');
+        res.render('./pages/aboutMe/boy-scouts.ejs');
     });
     
     app.get('/aboutMe/goals', function(req, res) {
-        res.render('./pages/goals.ejs');
+        res.render('./pages/aboutMe/goals.ejs');
     });
     
     app.get('/aboutMe/personal-blog', function(req, res) {
-        res.render('./pages/personal-blog.ejs');
+        res.render('./pages/aboutMe/personal-blog.ejs');
     });
     
+    
+    //OTHER
     app.get('/download', function(req, res) {
         var file = "public/pdf/Aaron Hammer's Resume.pdf"
         res.download(file); // Set disposition and send it.
