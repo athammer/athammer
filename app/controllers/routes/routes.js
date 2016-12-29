b57e8ade54d5db7a92b476e0a5cb67abc8dab027
Aaron Hammer <athammer@buffalo.edu>
2016-12-28 23:24:33 -0500

    Changed downloads to resume and updated resume.

--- app/controllers/routes/routes.js
+++ app/controllers/routes/routes.js
@@ -1,63 +1,63 @@
 
-module.exports = function(app){ 
-    
+module.exports = function(app){
+
     app.get('/', function(req, res) {
         res.render('./pages/main.ejs');
     });
-    
+
     app.get('/about', function(req, res) {
         res.render('./pages/about.ejs');
     });
-    
-    
-    
-    
+
+
+
+
     //PROJECTS
     app.get('/projects/13chan', function(req, res) {
         res.render('./pages/projects/13chan.ejs');
     });
-    
+
     app.get('/projects/ELCE', function(req, res) {
         res.render('./pages/projects/ELCE.ejs');
     });
-    
+
     app.get('/projects/athammer', function(req, res) {
         res.render('./pages/projects/athammer.ejs');
     });
     app.get('/projects/eagle-scout', function(req, res) {
         res.render('./pages/projects/eagle-scout.ejs');
     });
-    
+
     //ABOUTME
     app.get('/aboutMe/hobbies', function(req, res) {
         res.render('./pages/aboutMe/hobbies.ejs');
     });
-    
+
     app.get('/aboutMe/education', function(req, res) {
         res.render('./pages/aboutMe/education.ejs');
     });
-    
+
     app.get('/aboutMe/boy-scouts', function(req, res) {
         res.render('./pages/aboutMe/boy-scouts.ejs');
     });
-    
+
     app.get('/aboutMe/goals', function(req, res) {
         res.render('./pages/aboutMe/goals.ejs');
     });
-    
+
     app.get('/aboutMe/personal-blog', function(req, res) {
         res.render('./pages/aboutMe/personal-blog.ejs');
     });
-    
-    
+
+
     //OTHER
-    app.get('/download', function(req, res) {
+    app.get('/resume', function(req, res) {
         var file = "public/pdf/Aaron Hammer's Resume.pdf"
         res.download(file); // Set disposition and send it.
     });
-    
+
     app.get('*', function(req, res){
         res.status('404').render('./pages/404.ejs');
     });
-    
+
 };
