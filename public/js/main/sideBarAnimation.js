e94940d97d4a67ff2c058cf68f31e60b41536271
Aaron Hammer <athammer@buffalo.edu>
2017-01-24 21:14:25 -0500

    Added some more icons and added jquery animation.

--- public/js/main/sideBarAnimation.js
+++ public/js/main/sideBarAnimation.js
@@ -1,13 +1,23 @@
+$( document ).ready(function() {
+  console.log("doc ready");
+  var toggled = true;
 
-var toggled = true;
+  $(".toggleBar").click(function(){
+    console.log("toggle clicked.")
+    if(toggled) {
+      toggled=false;
+      $(".icon-container").hide();
+      $("#toggle").attr('class', 'fa fa-toggle-off');
+      $(".icons").animate({width: "5px"}, "fast");
 
-$("#left").click(function(){
-  console.log("toggle clicked.")
-  if(toggled) {
-    toggled=false;
-    $(this).animate({width: "20px"}, "fast");
-  }else {
-    toggled=true;
-    $(this).animate({width: "200px"}, "fast");
-  }
+
+
+    }else {
+      toggled=true;
+      $(".icon-container").show();
+      $("#toggle").attr('class', 'fa fa-toggle-on');
+      $(".icons").animate({width: "100px"}, "fast");
+
+    }
+  });
 });
