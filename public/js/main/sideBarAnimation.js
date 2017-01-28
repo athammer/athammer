38c4bf87e8638af628ff881359f5fe0ccdf93d86
Aaron Hammer <athammer@buffalo.edu>
2017-01-28 00:26:44 -0500

    Fixed side bar almost to compeletion, fixed the animations and made sure if it did get stuck it fixed itself.

--- public/js/main/sideBarAnimation.js
+++ public/js/main/sideBarAnimation.js
@@ -1,37 +1,98 @@
+//change left not icons as icons is percentage based
+
+
 $( document ).ready(function() {
   var toggled = true;
   var toggle1 = true;
+  var running = false;
 
   $(".toggleBar").click(function(){
     console.log("toggle clicked.")
     if(toggled) {
       toggled=false;
+      running=true;
       $(".icon-container").hide();
       $("#toggle").attr('class', 'fa fa-toggle-off');
-      $(".icons").animate({width: "5px"}, "fast");
+      $(".left").animate({width: "5px"}, "fast");
+      $(".left:animated").promise().done(function() {
+        running = false;
+      });
     }else {
       toggled=true;
+      running=true;
       $(".icon-container").show();
       $("#toggle").attr('class', 'fa fa-toggle-on');
-      $(".icons").animate({width: "55px"}, "fast");
+      $(".left").animate({width: "55px"}, "fast");
+      $(".left:animated").promise().done(function() {
+        running = false;
+      });
     }
   });
 
-  
-  $(".left").mouseover(function(){
-    console.log("side bar hovered");
-    $(".left").animate({width: "85px"}, "fast");
-    // $(".left").css({
-    //   "width": "85px",
-    // });
+
+  $("div.icons").mouseover(function(){
+    var pixels = $(".icons").css("width");
+    var open = parseInt(pixels, 10) > 54;
+    if(toggle1 && !running && open){
+      running = true;
+      console.log("side bar hovered");
+      $(".left").animate({width: "155px"}, "fast");
+      $("#home").append("<p class=\"side-bar-text sb\">Home</p>");
+      $("#about").append("<p class=\"side-bar-text sb\">About Me</p>");
+      $("#git").append("<p class=\"side-bar-text sb\">GitHub</p>");
+      $("#twitter").append("<p class=\"side-bar-text sb\">Twitter</p>");
+      $("#resume").append("<p class=\"side-bar-text sb\">Resume</p>");
+      $("#hobbies").append("<p class=\"side-bar-text sb\">Hobbies</p>");
+      $("#skills").append("<p class=\"side-bar-text sb\">Skills</p>");
+      $("#contact").append("<p class=\"side-bar-text sb\">Contact Me</p>");
+      $(".icons").css({
+        "text-align": "left",
+      });
+      $(".left:animated").promise().done(function() {
+        toggle1 = false;
+        running = false;
+      });
+    }
   });
 
-  $(".left").mouseout(function(){
-    console.log("side bar left");
-    $(".left").animate({width: "55px"}, "fast");
-
-    // $(".left").css({
-    //   "width": "55px",
-    // });
+  $("div.icons").mouseleave(function(){ //mouseout vs mouseleave, former runs in children, mouseleave doesnt
+    var pixels = $(".icons").css("width");
+    var open = parseInt(pixels, 10) > 54;
+    if(!toggle1 && !running && open){
+      running = true;
+      console.log("side bar left");
+      $( ".side-bar-text" ).remove();
+      $(".left").animate({width: "55px"}, "fast");
+      $(".icons").css({
+        "text-align": "center",
+      });
+      $(".left:animated").promise().done(function() {
+        toggle1 = true;
+        running = false;
+      });
+    }
   });
+
+
+  //ewwww, fixed frame being stuck open, even though scrolling on it will fix it(kinda)
+  var i = 0;
+  function fixSB() {
+    var pixels = $(".icons").css("width");
+    var open = parseInt(pixels, 10) > 54;
+    if(!$('div.icons').is(":hover") && !toggle1 && !running && open){
+      running = true;
+      console.log("side bar left");
+      $( ".side-bar-text" ).remove();
+      $(".left").animate({width: "55px"}, "fast");
+      $(".icons").css({
+        "text-align": "center",
+      });
+      $(".left:animated").promise().done(function() {
+        toggle1 = true;
+        running = false;
+      });
+    }
+    setTimeout(fixSB, 1000); //quarter a second
+  }
+  setTimeout(fixSB, 0);
 });
