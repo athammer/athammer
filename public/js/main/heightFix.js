21ebb34f159984f2d2cbfecd0fa63b4528876715
Aaron Hammer <athammer@buffalo.edu>
2017-03-02 23:03:26 -0500

    Fixed some issues with javascript, redid titles, and added a footer.

--- public/js/main/heightFix.js
+++ public/js/main/heightFix.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {
+//  $('.main').css("width", $( document  ).width() - 20);
+  $('.main').css("height", $( document  ).height() - 92);
   $( window ).resize(function() {
-    $('.main').css("width", $( document  ).width() - 20);
+//    $('.main').css("width", $( document  ).width() - 20);
     $('.main').css("height", $( document  ).height() - 92);
-  }
+  });
 });
+//mobile has to sroll
