a45e09270d62691cabb0c289efd04d929c5dea96
athammer <athammer@buffalo.edu>
2017-08-04 16:43:22 +0000

    change to socket.io

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var subdomain = require('express-subdomain');
 var router = express.Router();
 var favicon = require('serve-favicon');
 var helmet = require('helmet');
-var helmet = require('helmet');
-var socketCluster = require('socketcluster');
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+
+
+io.on('connection', function(client){
+  client.on('event', function(data){});
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  client.on('disconnect', function(){});
+});
+
 
 
 
@@ -15,39 +26,6 @@ var middlewares = require("./app/middleware/middleware.js");
 app.set('view engine', 'ejs');
 
 
-var api_credentials =
-  "apiKey"    : process.env.KEY_COINIGY,
-  "apiSecret" : process.env.SECRET_COINIGY
-}
-
-var options = {
-  hostname  : "sc-02.coinigy.com",
-  port      : "443",
-  secure    : "true"
-};
-
-var SCsocket = socketCluster.connect(options);
-
-
-SCsocket.on('connect', function (status) {
-
-  console.log(status);
-
-  SCsocket.on('error', function (err) {
-        console.log(err);
-  });
-
-
-  SCsocket.emit("auth", api_credentials, function (err, token) {
-
-      if (!err && token) {
-
-
-      } else {
-          console.log(err)
-      }
-  });
-});
 
 
 
